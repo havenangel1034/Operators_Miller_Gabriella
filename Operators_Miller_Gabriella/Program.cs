d0db00a01847af8bdc4c412aa8589e829c78f46b
Gabby <gabbie10112005@gmail.com>
2024-12-04 10:46:37 -0600

    a

--- Operators_Miller_Gabriella/Program.cs
+++ Operators_Miller_Gabriella/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Operators_Miller_Gabriella
+{
+        class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //for the integer data type
+
+            //initializes an integer that multiplies two integers
+            int intMult = 40 * 8;
+            //initializes an integer that adds to integers
+            int intAdd = 70 + 22;
+            //prints the maximum and minimum of these two variables
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}. The min of the two is {Math.Min(intMult, intAdd)}");
+            //initalizes boolean to see if intMult is greater than intAdd
+            bool isIntGreater = intMult > intAdd;
+            //prints if intMult is greater than intAdd
+            Console.WriteLine($"{intMult} is greater than {intAdd} {isIntGreater}.");
+
+
+            //for the short data type
+
+            //initializes an integer that multiplies two integers
+            int intMult = 40 * 8;
+            //initializes an integer that adds to integers
+            int intAdd = 70 + 22;
+            //prints the maximum and minimum of these two variables
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}. The min of the two is {Math.Min(intMult, intAdd)}");
+            //initalizes boolean to see if intMult is greater than intAdd
+            bool isIntGreater = intMult > intAdd;
+            //prints if intMult is greater than intAdd
+            Console.WriteLine($"{intMult} is greater than {intAdd} {isIntGreater}.");
+
+            //for the long data type
+
+            //initializes an integer that multiplies two integers
+            int intMult = 40 * 8;
+            //initializes an integer that adds to integers
+            int intAdd = 70 + 22;
+            //prints the maximum and minimum of these two variables
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}. The min of the two is {Math.Min(intMult, intAdd)}");
+            //initalizes boolean to see if intMult is greater than intAdd
+            bool isIntGreater = intMult > intAdd;
+            //prints if intMult is greater than intAdd
+            Console.WriteLine($"{intMult} is greater than {intAdd} {isIntGreater}.");
+
+            //for the float data type
+
+            //initializes an integer that multiplies two integers
+            int intMult = 40 * 8;
+            //initializes an integer that adds to integers
+            int intAdd = 70 + 22;
+            //prints the maximum and minimum of these two variables
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}. The min of the two is {Math.Min(intMult, intAdd)}");
+            //initalizes boolean to see if intMult is greater than intAdd
+            bool isIntGreater = intMult > intAdd;
+            //prints if intMult is greater than intAdd
+            Console.WriteLine($"{intMult} is greater than {intAdd} {isIntGreater}.");
+
+            //for the double data type
+
+            //initializes an integer that multiplies two integers
+            int intMult = 40 * 8;
+            //initializes an integer that adds to integers
+            int intAdd = 70 + 22;
+            //prints the maximum and minimum of these two variables
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}. The min of the two is {Math.Min(intMult, intAdd)}");
+            //initalizes boolean to see if intMult is greater than intAdd
+            bool isIntGreater = intMult > intAdd;
+            //prints if intMult is greater than intAdd
+            Console.WriteLine($"{intMult} is greater than {intAdd} {isIntGreater}.");
+
+            //for the decimal data type
+
+            //initializes an integer that multiplies two integers
+            int intMult = 40 * 8;
+            //initializes an integer that adds to integers
+            int intAdd = 70 + 22;
+            //prints the maximum and minimum of these two variables
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}. The min of the two is {Math.Min(intMult, intAdd)}");
+            //initalizes boolean to see if intMult is greater than intAdd
+            bool isIntGreater = intMult > intAdd;
+            //prints if intMult is greater than intAdd
+            Console.WriteLine($"{intMult} is greater than {intAdd} {isIntGreater}.");
+        }
+    }
+}
